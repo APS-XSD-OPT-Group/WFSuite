352543e2e15e4552b1dd8567ead768d900822385
jkirchman <jkirchman@anl.gov>
2025-02-05 15:47:33 -0600

    3-ID experiments

--- aps/wavefront_analysis/absolute_phase/legacy/process_images_executor.py
+++ aps/wavefront_analysis/absolute_phase/legacy/process_images_executor.py
@@ -1231,8 +1231,11 @@ def execute_process_image(**arguments):
         if args.det_size[0] % args.rebinning != 0: raise ValueError(f"Incompatible shape: det_size[0] {args.det_size[0]} is not divisible by the rebinning factor {args.rebinning}")
         if args.det_size[1] % args.rebinning != 0: raise ValueError(f"Incompatible shape: det_size[1] {args.det_size[1]} is not divisible by the rebinning factor {args.rebinning}")
 
-        para_simulation['det_size'] = [int(args.det_size[0] / args.rebinning), int(args.det_size[1] / args.rebinning)]
-        para_simulation['p_x']      = args.p_x * args.rebinning
+        args.det_size = [int(args.det_size[0] / args.rebinning), int(args.det_size[1] / args.rebinning)]
+        args.p_x     *= args.rebinnin
+
+        para_simulation['det_size'] = args.det_size
+        para_simulation['p_x']      = args.p_x
 
     if args.dark is None: dark = np.zeros(I_img_raw.shape)
     else:                 dark = load_image(args.dark)
