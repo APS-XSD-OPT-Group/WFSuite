ac978e154550a4be6590201150768148f9aaab9c
lucarebuffi <lrebuffi@anl.gov>
2025-04-29 17:06:09 -0500

    bug fixing

--- aps/wavefront_analysis/absolute_phase/legacy/process_images_executor.py
+++ aps/wavefront_analysis/absolute_phase/legacy/process_images_executor.py
@@ -1184,14 +1184,14 @@ def execute_process_image(**arguments):
 
     if args.dark is None: dark = np.zeros(I_img_raw.shape)
     else:
-        if os.path.splitext(str(args.dark).lower()) == "json": dark = load_stream_image(args.dark)
+        if os.path.splitext(str(args.dark).lower())[1] == ".json": dark = load_stream_image(args.dark)
         else:                                                  dark = load_image(args.dark)
 
         if args.rebinning > 1: _, _, dark = rebin_2D(None, None, dark, args.rebinning, exact=True)
 
     if args.flat is None: flat = snd.uniform_filter(I_img_raw, size = 10 * (args.pattern_size / args.p_x))  # XSHI Feb 2024 change from 5 to 10
     else:
-        if os.path.splitext(str(args.flat).lower()) == "json": flat = load_stream_image(args.flat)
+        if os.path.splitext(str(args.flat).lower())[1] == ".json": flat = load_stream_image(args.flat)
         else:                                                  flat = load_image(args.flat)
 
         if args.rebinning > 1: _, _, flat = rebin_2D(None, None, flat, args.rebinning, exact=True)
